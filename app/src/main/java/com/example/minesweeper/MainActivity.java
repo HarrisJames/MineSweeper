e349d572450802885c4aeb52986363cba5ea3a9e
JameswHarris <jameswha@usc.edu>
2022-09-19 00:13:03 -0700

    Display Grid

--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.minesweeper;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.gridlayout.widget.GridLayout;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<TextView> cell_tvs;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cell_tvs = new ArrayList<>();
+
+        LayoutInflater li = LayoutInflater.from(this);
+        GridLayout grid = findViewById(R.id.gridLayout01);
+        for (int i = 0; i<=9; i++) {
+            for (int j=0; j<=7; j++) {
+                TextView tv = (TextView) li.inflate(R.layout.custom_cell_layout, grid, false);
+                tv.setTextColor(Color.GRAY);
+                tv.setBackgroundColor(Color.GRAY);
+                //tv.setOnClickListener(this::onClickTV);
+
+                GridLayout.LayoutParams lp = (GridLayout.LayoutParams) tv.getLayoutParams();
+                lp.rowSpec = GridLayout.spec(i);
+                lp.columnSpec = GridLayout.spec(j);
+
+                grid.addView(tv, lp);
+
+                cell_tvs.add(tv);
+            }
+        }
     }
 }
